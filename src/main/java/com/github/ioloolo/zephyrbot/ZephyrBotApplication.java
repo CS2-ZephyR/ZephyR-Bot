8207079a67be4622f7a85e5972d2d9fbee1581f2
Son Daehyeon <sondaehyeon01@gmail.com>
2024-03-30 17:28:25 +0900

    Add webpage advertise messsage

--- src/main/java/com/github/ioloolo/zephyrbot/ZephyrBotApplication.java
+++ src/main/java/com/github/ioloolo/zephyrbot/ZephyrBotApplication.java
@@ -1,13 +1,18 @@
 package com.github.ioloolo.zephyrbot;
 
+import java.awt.*;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 
 import com.github.ioloolo.zephyrbot.interaction.button.ButtonBridge;
 import com.github.ioloolo.zephyrbot.interaction.command.CommandBridge;
@@ -36,6 +41,9 @@ public class ZephyrBotApplication {
 		@Value("${discord.token}")
 		private String token;
 
+		@Value("${discord.advertise-channel}")
+		private long advertiseChannel;
+
 		@Getter
 		private JDA jda;
 
@@ -54,7 +62,32 @@ public class ZephyrBotApplication {
 					.build()
 					.awaitReady();
 
+			sendWebAd();
+
 			log.info("Success fetch bot data. ({})", jda.getSelfUser().getName());
+		}
+
+		private void sendWebAd() {
+
+			TextChannel channel = jda.getTextChannelById(advertiseChannel);
+			if (channel == null) {
+				return;
+			}
+
+			channel.retrievePinnedMessages().queue(pinned -> {
+				if (!pinned.isEmpty()) {
+					return;
+				}
+
+				MessageEmbed messageEmbed = new EmbedBuilder().setTitle("웹페이지")
+						.addField(new MessageEmbed.Field("주소", "http://teamzephyr.kro.kr", true))
+						.setThumbnail("https://cdn.discordapp.com/app-icons/1081544900539076639/f58a0423c26e5fa72c1d1bbdc95f5311.png")
+						.setFooter("Team ZephyR")
+						.setColor(Color.YELLOW)
+						.build();
+
+				channel.sendMessageEmbeds(messageEmbed).queue(message -> channel.pinMessageById(message.getIdLong()).queue());
+			});
 		}
 	}
 }
